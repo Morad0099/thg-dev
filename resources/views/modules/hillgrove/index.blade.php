567751794480ef1fc4f15545b85e99a2ed1e7367
Musah Morad <musahmorad01@gmail.com>
2024-06-18 18:26:09 -0400

    "Updated CSS styles and removed exterior image gallery from multiple estate modules"

--- resources/views/modules/hillgrove/index.blade.php
+++ resources/views/modules/hillgrove/index.blade.php
@@ -5,24 +5,15 @@
     <h1>HILLGROVE ESTATE</h1>
     <div class="content-container">
         <div class="image-section">
-            {{-- <h2 style="margin-left: 50px">Gallery</h2>
-            <h3 style="margin-left: 50px">Interior</h3> --}}
             <div class="gallery-container">
                 <button class="arrow prev" onclick="prevImage('interior')">&#9664;</button>
                 <img id="interior-image" src="{{ asset('images/interior/' . $interiorImages[0]) }}" alt="Interior Image" class="gallery-image lazyload">
                 <button class="arrow next" onclick="nextImage('interior')">&#9654;</button>
             </div>
-
-            {{-- <h3 style="margin-left: 50px; margin-top: 10px">Exterior</h3>
-            <div class="gallery-container">
-                <button class="arrow prev" onclick="prevImage('exterior')">&#9664;</button>
-                <img id="exterior-image" src="{{ asset('images/' . $exteriorImages[0]) }}" alt="Exterior Image" class="gallery-image">
-                <button class="arrow next" onclick="nextImage('exterior')">&#9654;</button>
-            </div> --}}
         </div>
         <div class="text-section">
             <p class="description">
-                Hillgrove Estate offers luxury living with modern amenities and beautiful surroundings.
+                HillGrove Estate offers luxury living with modern amenities and beautiful surroundings.
                 Located in the heart of the city, it provides easy access to all major attractions and facilities.
             </p>
             <div class="features">
@@ -35,7 +26,7 @@
                     <li>Swimming pool</li>
                 </ul>
             </div>
-            <div class="but-container">
+            <div class="button-container">
                 <a href="https://wa.me/233558871838"><button class="btn btn-primary">BUY NOW</button></a>
             </div>
         </div>
@@ -45,16 +36,12 @@
 
 <script>
     let interiorIndex = 0;
-    let exteriorIndex = 0;
 
     const interiorImages = @json($interiorImages);
-    const exteriorImages = @json($exteriorImages);
 
     function showImage(type, index) {
         if (type === 'interior') {
             document.getElementById('interior-image').src = `{{ asset('images/interior/') }}/${interiorImages[index]}`;
-        } else {
-            document.getElementById('exterior-image').src = `{{ asset('images/') }}/${exteriorImages[index]}`;
         }
     }
 
@@ -62,9 +49,6 @@
         if (type === 'interior') {
             interiorIndex = (interiorIndex > 0) ? interiorIndex - 1 : interiorImages.length - 1;
             showImage(type, interiorIndex);
-        } else {
-            exteriorIndex = (exteriorIndex > 0) ? exteriorIndex - 1 : exteriorImages.length - 1;
-            showImage(type, exteriorIndex);
         }
     }
 
@@ -72,9 +56,6 @@
         if (type === 'interior') {
             interiorIndex = (interiorIndex < interiorImages.length - 1) ? interiorIndex + 1 : 0;
             showImage(type, interiorIndex);
-        } else {
-            exteriorIndex = (exteriorIndex < exteriorImages.length - 1) ? exteriorIndex + 1 : 0;
-            showImage(type, exteriorIndex);
         }
     }
 </script>
