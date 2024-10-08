4e3b29aa1b2f990307183a75ba3457353f56380e
Musah Morad <musahmorad01@gmail.com>
2024-10-08 16:00:48 -0400

    "Updated image file extensions from .jpg to .png in navbar and index views."

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -102,13 +102,13 @@
                 <a href="{{ route('hillgrove') }}" class="link-text">More Details</a>
             </div>
             <div class="estate">
-              <a href="{{ route('lucas_court') }}">  <img src="images/lucas.jpg" alt="Estate 1" class="estate-img"></a>
+              <a href="{{ route('lucas_court') }}">  <img src="images/lucas.png" alt="Estate 1" class="estate-img"></a>
                 <h6 class="estate-name">Lucas Court</h6>
                 <a href="https://wa.me/233558871838" class="link-text">Buy Now</a>
                 <a href="{{ route('lucas_court') }}" class="link-text">More Details</a>
             </div>
             <div class="estate">
-             <a href="{{ route('hill_rise_villas') }}">  <img src="/images/hillrise.jpg" alt="Estate 1" class="estate-img"></a> 
+             <a href="{{ route('hill_rise_villas') }}">  <img src="/images/hillrise.png" alt="Estate 1" class="estate-img"></a> 
                 <h6 class="estate-name">HillRise Villas</h6>
                 <a href="https://wa.me/233558871838" class="link-text">Buy Now</a>
                 <a href="{{ route('hill_rise_villas') }}" class="link-text">More Details</a>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <div class="carousel-container">
         <div class="carousel-slide">
-            <img src="/images/hillrise.jpg" alt="Image 1">
-            <img src="/images/lucas.jpg" alt="Image 2">
-            <img src="/images/thesas.jpg" alt="Image 3">
+            <img src="/images/hillrise.png" alt="Image 1">
+            <img src="/images/lucas.png" alt="Image 2">
+            <img src="/images/thesas.png" alt="Image 3">
             <img src="/images/Queens.png" alt="Image 4">
             <img src="/images/Artboard 1.jpg" alt="Image 5">
         </div>
